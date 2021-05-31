f756c0bb4632e9544506d84169db8094e969c107
zerouatiyousra <84454995+zerouatiyousra@users.noreply.github.com>
2021-06-01 00:51:49 +0100

    Add files via upload

---
+++ R/Rdist.R
@@ -0,0 +1,40 @@
+#' simulation d'une loi discrete
+#' @export
+#' @param x numeric vector representing the values of the random variable
+#' @param p numeric vector representing the probabilities
+rdist<-function(x,p)
+{
+  n=length(p)
+  r=runif(1)
+  b=p[1]
+  if((r>0)&(r<=b))
+  {
+    y=x[1]
+    return(y)
+  }
+  else
+  {
+    a=p[1]
+    b=b+p[2]
+    for(i in 2:n-1)
+    {
+      if((r>=a)&(r<=b))
+      {
+        y=x[i]
+        return(y)
+      }
+      else
+      {
+        a=b
+        b=b+p[i+1]
+        i=i+1
+      }
+    }
+    y=x[n]
+    return(y)
+  }
+}
+
+
+
+
